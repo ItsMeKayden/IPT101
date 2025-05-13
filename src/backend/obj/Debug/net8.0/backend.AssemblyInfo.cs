ba1576413848b4d29df0670a2d87489fd24ac049
KHUN <benegas.johnandrei.gutierrez1@gmail.com>
2025-05-13 18:56:23 +0800

    backup

--- src/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ src/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f125d12990c08a49eca0b3aa0c82319972360ec")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cfe9da2d7c5079b816994a69981b15808aa7eb")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
